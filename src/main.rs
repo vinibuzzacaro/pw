fa37f581a34edfc3f961bc9735ec45522d94cf73
Vinicius Buzzacaro <vinicius.buzzacaro@gmail.com>
2025-08-26 23:41:13 -0300

    Added tag functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashSet, fs, io::{self, Read}};
+use std::{collections::HashSet, fs, io::{self}};
 
 use anyhow::anyhow;
+use arboard::Clipboard;
 use clap::{Parser, Subcommand};
-use copypasta::{ClipboardContext, ClipboardProvider};
 use keyring::Entry;
 use serde::{Deserialize, Serialize};
 
@@ -17,23 +17,36 @@ struct Cli {
     #[arg(short, long, global(true))]
     copy: bool,
     #[arg(short, long, global(true))]
-    quiet: bool
+    quiet: bool,
+    #[arg(short, long, global(true))]
+    tag: Option<String>
 }
 
 #[derive(Subcommand)]
 enum Commands {
     Set {
         key: String,
-        password: String
+        password: String,
+        #[arg(short, long, global(true))]
+        tag: Option<String>
     },
     #[command(name("rm"))]
-    Remove { key: String },
-    List
+    Remove { 
+        key: String, 
+        #[arg(short, long, global(true))]
+        tag: Option<String> 
+    },
+    List { 
+        #[arg(short, long, global(true))]
+        tag: Option<String>,
+        #[arg(long("no-tag"), conflicts_with("tag"))]
+        no_tag: bool
+    } 
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 struct KeyStorage {
-    keys: HashSet<String>
+    keys: HashSet<(String, Option<String>)>
 }
 
 impl KeyStorage {
@@ -58,64 +71,136 @@ impl Cli {
         Entry::new_with_target(key, SERVICE, &whoami::username())
     }
 
-    fn handle_set_command(key: String, password: String, quiet: bool) -> anyhow::Result<()> {
+    fn handle_set_command(key: String, password: String, quiet: bool, tag: Option<String>) -> anyhow::Result<()> {
         let mut storage = KeyStorage::load()?;                
-        let entry = Self::entry(&key)?;
-        entry.set_password(&password)?;
-        if !quiet {
-            println!("Password for \"{key}\" set successfully.");
+        let entry_name = match &tag {
+            Some(tag) => format!("{key}:{tag}"),
+            None => key.to_string(),
+        };
+        let entry = Self::entry(&entry_name)?;
+        entry.set_password(&password)?;                        
+        if storage.keys.insert((key, tag)) {
+            storage.save()?;
         }
-        if storage.keys.insert(key) {
-            storage.save()?;            
-        }                        
+        if !quiet {
+            println!("Password for \"{entry_name}\" set successfully.");
+        }
         Ok(())
     }
 
-    fn handle_password(key: String, quiet: bool, copy: bool) -> anyhow::Result<()> {
-        let entry = Self::entry(&key)?;
+    fn handle_password(key: String, quiet: bool, copy: bool, tag: Option<String>) -> anyhow::Result<()> {        
+        let matched_keys = KeyStorage::load()?.keys
+            .iter()
+            .filter_map(|(k, t)| (k == &key).then(|| {                
+                match &tag {
+                    Some(tag) => ((k.to_string(), t.to_owned()), format!("{key}:{tag}")),
+                    None => match t {
+                        Some(tag_str) => ((k.to_string(), t.to_owned()), format!("{key}:{tag_str}")),
+                        None => ((k.to_string(), t.to_owned()), k.to_string()),
+                    }
+                }
+            }))
+            .collect::<Vec<((String, Option<String>), String)>>();        
+        if matched_keys.len() > 1 && tag.is_none() {
+            let matches = matched_keys
+                .into_iter()
+                .map(|(_, key)| format!("* {key}"))
+                .collect::<Vec<String>>()
+                .join("\n");            
+            println!("Multiple matches found for \"{key}\":\n{matches}\nUse --tag/-t to specify which one to read from");
+            return Ok(())
+        }
+        let Some((_, entry_name)) = matched_keys.first() else {
+            panic!("Key \"{key}\" was found initially, but returned None.");
+        };
+        let entry = Self::entry(&entry_name)?;
         let pw = entry.get_password()?;                
         if !quiet {
-            println!("The password for \"{key}\" is \"{pw}\".");
+            println!("The password for \"{entry_name}\" is \"{pw}\".");
         }
         if copy {
-            let mut ctx = ClipboardContext::new()
+            let mut clipboard = Clipboard::new()
                 .map_err(|e| anyhow!("Failed to initialize clipboard: {e}"))?;
-            ctx.set_contents(pw)
+            clipboard.set_text(pw)
                 .map_err(|e| anyhow!("Failed to set clipboard content: {e}"))?;
             if !quiet {
                 println!("Copied to the clipboard!");
             }
-
-        }         
+        }
         Ok(())
     }
 
-    fn handle_list_command(quiet: bool) -> anyhow::Result<()> {
+    fn handle_list_command(quiet: bool, tag: Option<String>, no_tag: bool) -> anyhow::Result<()> {
         if !quiet {            
             let storage = KeyStorage::load()
                 .unwrap_or_default();
             if !storage.keys.is_empty() {
-                let keys = storage.keys
-                    .into_iter()
-                    .map(|s| format!("* {s}"))
+                let filtered_keys = match (&tag, &no_tag) {
+                    (Some(_), false) => storage.keys
+                        .into_iter()
+                        .filter_map(|(k, t)| (&t == &tag).then(|| (k, t)))
+                        .collect::<Vec<(String, Option<String>)>>(),
+                    (_, true) => storage.keys
+                        .into_iter()
+                        .filter_map(|(k, t)| t.is_none().then(|| (k, t)))
+                        .collect::<Vec<(String, Option<String>)>>(),
+                    (None, false) => storage.keys.into_iter().collect(),                    
+                };
+                let keys = filtered_keys
+                    .into_iter()                    
+                    .map(|(key, tag)| {
+                        if let Some(tag) = tag {
+                            format!("* {key}:{tag}")
+                        } else {
+                            format!("* {key}")
+                        }
+                    })
                     .collect::<Vec<String>>()
                     .join("\n");
                 println!("{keys}");
             }        
-        }        
+        }
         Ok(())
     }
 
-    fn handle_remove_command(key: String, quiet: bool) -> anyhow::Result<()> {
-        let mut storage = KeyStorage::load()?;
-        if storage.keys.remove(&key) {
-            let entry = Self::entry(&key)?;                     
-            entry.delete_credential()?;                
-            storage.save()?;
-        }
+    fn handle_remove_command(key: String, quiet: bool, tag: Option<String>) -> anyhow::Result<()> {        
+        let mut storage = KeyStorage::load()?;        
+        let matched_keys = storage.keys
+            .iter()
+            .filter_map(|(k, t)| (k == &key).then(|| {                
+                match &tag {
+                    Some(tag) => ((k.to_string(), t.to_owned()), format!("{key}:{tag}")),
+                    None => match t {
+                        Some(tag_str) => ((k.to_string(), t.to_owned()), format!("{key}:{tag_str}")),
+                        None => ((k.to_string(), t.to_owned()), k.to_string()),
+                    }
+                }
+            }))
+            .collect::<Vec<((String, Option<String>), String)>>();        
+        if matched_keys.len() > 1 && tag.is_none() {
+            let matches = matched_keys
+                .into_iter()
+                .map(|(_, key)| format!("* {key}"))
+                .collect::<Vec<String>>()
+                .join("\n");            
+            println!("Multiple matches found for \"{key}\":\n{matches}\nUse --tag/-t to specify which one to remove");
+            return Ok(())
+        }    
+        let msg = match matched_keys.first() {
+            Some((storage_key, key_str)) => {
+                if !storage.keys.remove(&storage_key) {
+                    panic!("Key found in storage previously failed on removal.");
+                }
+                let entry = Self::entry(&key_str)?;
+                entry.delete_credential()?;
+                storage.save()?;
+                format!("\"{key_str}\" removed successfully.")
+            },
+            None => format!("\"{key}\" not found."),                     
+        };
         if !quiet {
-            println!("\"{key}\" removed successfully.");
-        }  
+            println!("{msg}");
+        }
         Ok(())
     }
 }
@@ -123,11 +208,14 @@ impl Cli {
 fn main() {
     let cli = Cli::parse();
     let err = match cli.command {
-        Some(Commands::Set { key, password }) => Cli::handle_set_command(key, password, cli.quiet),
-        Some(Commands::List) => Cli::handle_list_command(cli.quiet),
-        Some(Commands::Remove { key}) => Cli::handle_remove_command(key, cli.quiet),
+        Some(Commands::Set { key, password, tag }) 
+            => Cli::handle_set_command(key, password, cli.quiet, tag),
+        Some(Commands::List { tag, no_tag }) 
+            => Cli::handle_list_command(cli.quiet, tag, no_tag),
+        Some(Commands::Remove { key, tag }) 
+            => Cli::handle_remove_command(key, cli.quiet, tag),
         None => match cli.key {
-            Some(key) => Cli::handle_password(key, cli.quiet, cli.copy),
+            Some(key) => Cli::handle_password(key, cli.quiet, cli.copy, cli.tag),
             None => return,
         }
     };
